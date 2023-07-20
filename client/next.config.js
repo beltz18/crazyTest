667dfdaca04e853210d470629056207f4835b648
Beltz Anhxton <beltz.18kyodai@gmail.com>
2023-07-20 01:03:14 -0400

    added dashboard styled component
    created a dashboard component with tailwind

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'flowbite.s3.amazonaws.com',
+      },
+    ],
+  },
+}
 
 module.exports = nextConfig
--- client/src/pages/dashboard/index.tsx
+++ client/src/pages/dashboard/index.tsx
@@ -1,29 +1,21 @@
 import '@s/main.css'
+
 import React       from 'react'
 import MyHead      from '@c/MyHead'
+import MyNavbar    from '@c/Navbar'
+import MyFeed      from '@c/Feed'
+import MyFooter    from '@r/components/Footer'
 import { raleway } from '@c/fonts'
 
-const Dashboard = ({ title }: any) => {
+const Dashboard = () => {
   return (
     <>
-      <MyHead title="crazyTest" />
-      <h1 className={raleway.className}>Hello, { title }</h1>
-      <p className={raleway.className}>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-        At in omnis reprehenderit magni inventore architecto odio
-        ullam necessitatibus, ex dolorem quo beatae atque deleniti
-        minus eius ea quibusdam dolorum modi? 2023
-      </p>
+      <MyHead title="crazyTest"  />
+      <MyNavbar font={ raleway } />
+      <MyFeed   font={ raleway } />
+      <MyFooter font={ raleway } />
     </>
   )
 }
 
 export default Dashboard
-
-export async function getServerSideProps () {
-  return {
-    props: {
-      title: 'Dashboard'
-    }
-  }
-}
