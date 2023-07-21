f225f68acb5ad07e98ed9643a82fae0775076bca
Beltz Anhxton <beltz.18kyodai@gmail.com>
2023-07-21 13:42:04 -0400

    added open navbar through button in responsive design function

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -3,6 +3,8 @@ import Link  from 'next/link'
 import Image from 'next/image'
 
 const MyNavbar = ({ font, page }: any) => {
+  const [showNav, setShowNav] = React.useState(false)
+
   const logout = () => {
     localStorage.clear()
   }
@@ -22,14 +24,14 @@ const MyNavbar = ({ font, page }: any) => {
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Crazy Shop</span>
           </Link>
           
-          <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+          <button type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" onClick={() => { setShowNav(!showNav) }}>
             <span className="sr-only">Open main menu</span>
             <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
               <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
             </svg>
           </button>
 
-          <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+          <div className={`${!showNav ? "hidden" : ""} w-full md:block md:w-auto`}>
             <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-800 md:flex-row md:space-x-8 md:mt-0 md:border-0 dark:bg-white">
               <li key="home">
                 <Link href="/" className={`block py-2 pl-3 pr-4 ${page == 'home' ? "text-blue-500" : "text-white"} rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent`}>Home</Link>
